08b48b890356eec7bad271a29f933dd00d36c77f
cottoncandies <1790882721@qq.com>
2018-07-08 13:24:44 +0800

    完成jdbcutil类的编写

--- src/main/java/backup/JdbcBackup.java
+++ src/main/java/backup/JdbcBackup.java
@@ -3,7 +3,13 @@ package backup;
 import entity.*;
 import mysql.WriteToMysql;
 import postgresql.ReadFromPostgresql;
+import util.JDBCUtil;
 
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class JdbcBackup {
@@ -36,10 +42,36 @@ public class JdbcBackup {
 //            WriteToMysql.addAbility(ability);
 //        }
 
-        List<BookCatalog> bookCatalogs = ReadFromPostgresql.getBookCatalogs();
-        //写入mysql数据库
-        for (BookCatalog bookCatalog : bookCatalogs) {
-            WriteToMysql.addBookCatalog(bookCatalog);
+//        List<BookCatalog> bookCatalogs = ReadFromPostgresql.getBookCatalogs();
+//        //写入mysql数据库
+//        for (BookCatalog bookCatalog : bookCatalogs) {
+//            WriteToMysql.addBookCatalog(bookCatalog);
+//        }
+
+
+//        PreparedStatement pstm = null;
+//        Connection conn = null;
+//
+//
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//
+//            String url = "jdbc:mysql://localhost:3306/cmfz?useSSL=false";
+//            String username = "root";
+//            String password = "root";
+//            conn = DriverManager.getConnection(url, username, password);
+//            System.out.println("---------------------");
+//            System.out.println(conn);
+//        }catch (Exception e){
+//
+//        }
+
+        Connection connection = null;
+        try {
+            connection = JDBCUtil.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        System.out.println(connection);
     }
 }
