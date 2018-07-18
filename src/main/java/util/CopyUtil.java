786da3a5acce63a9bf77f8e8d0809db9622cac08
cottoncandies <1790882721@qq.com>
2018-07-18 15:06:30 +0800

    完成通用CopyUtil的设计

--- src/main/java/util/CopyUtil.java
+++ src/main/java/util/CopyUtil.java
@@ -11,37 +11,31 @@ public class CopyUtil {
 
         Connection postgresqlConn = null;
         Connection mysqlConn = null;
-
         PreparedStatement postgresqlPstm = null;
         PreparedStatement mysqlPstm = null;
-
         ResultSet rs = null;
-
         int count = 0;
 
         try {
             //1.获取Connection连接
             postgresqlConn = PostgresqlUtil.getConnection();
             mysqlConn = MysqlUtil.getConnection();
-
             //设置AutoCommit属性为false,这个一定要用
             //配合设置setFetchSize(10)   setFetchDirection(ResultSet.FETCH_FORWARD)等属性
             //避免大量数据读写时出现java.lang.OutOfMemoryError: Java heap space
             postgresqlConn.setAutoCommit(false);
             mysqlConn.setAutoCommit(false);
 
-
             // 2.获取SQL执行者
             postgresqlPstm = postgresqlConn.prepareStatement(selectSql, ResultSet.TYPE_FORWARD_ONLY,
                     ResultSet.CONCUR_READ_ONLY);
             postgresqlPstm.setFetchSize(1000);
             postgresqlPstm.setFetchDirection(ResultSet.FETCH_FORWARD);
-
             mysqlPstm = mysqlConn.prepareStatement(insertSql);
-
 
             // 3.执行sql语句
             rs = postgresqlPstm.executeQuery();
+            //获取数据库表的字段个数
             int columnCount = rs.getMetaData().getColumnCount();
 
             while (rs.next()) {
@@ -49,19 +43,19 @@ public class CopyUtil {
                 for (int i = 1; i <= columnCount; i++) {
                     mysqlPstm.setObject(i, rs.getString(i));
                 }
+                //添加到批操作
                 mysqlPstm.addBatch();
+                //每5000条insert语句发送给数据库并执行一次数据库操作
                 if (count % 5000 == 0) {
                     mysqlPstm.executeBatch();
                     mysqlConn.commit();
                     mysqlPstm.clearBatch();        //提交后，Batch清空。
                 }
             }
-
+            //不足5000条数据,将剩下的发送给数据库执行操作。
             mysqlPstm.executeBatch();
-            //优化插入第三步       提交，批量插入数据库中。
             mysqlConn.commit();
             mysqlPstm.clearBatch();
-
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -70,6 +64,4 @@ public class CopyUtil {
             MysqlUtil.close(mysqlConn, mysqlPstm, null);
         }
     }
-
-
 }
